a468743b7300cfc1a70b338b26d844a2bb09a789
Val Saven <val.saven@gmail.com>
2018-04-05 02:48:36 +0300

    Add channels list

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,55 @@
 <template>
-  <div id="app">
-    <v-app>
-      <h1>{{title}}</h1>
-      <v-form v-model="valid" ref="form" lazy-validation>
-        <v-switch
-          :label="`${isUsername ? 'По имени пользователя' : 'По ID канала'}`"
-          color="indigo"
-          v-model="isUsername"
-        ></v-switch>
-        <v-text-field
-          :label="`${isUsername ? 'Имя пользователя' : 'ID канала'}`"
-          v-model="search"
-          :rules="searchRules"
-          required
-        ></v-text-field>
-        <v-btn @click="submit" :disabled="!valid">Добавить</v-btn>
-        <v-btn @click="clear">Очистить</v-btn>
-      </v-form>
-    </v-app>
-  </div>
+<div id="app">
+  <v-app>
+    <v-container fliud fill-height grid-list-md text-xs-center>
+      <v-layout row wrap align-center justify-center>
+        <v-flex xs12>
+          <h1>{{title}}</h1>
+        </v-flex>
+        <v-flex xs12>
+          <v-form v-model="valid" ref="form" lazy-validation>
+            <v-switch :label="`${isUsername ? 'По имени пользователя' : 'По ID канала'}`" color="indigo" v-model="isUsername"></v-switch>
+            <v-text-field :label="`${isUsername ? 'Имя пользователя' : 'ID канала'}`" v-model="search" :rules="searchRules" required></v-text-field>
+            <v-btn @click="submit" :disabled="!valid">Добавить</v-btn>
+            <v-btn @click="clear">Очистить</v-btn>
+          </v-form>
+        </v-flex>
+        <v-flex xs12>
+          <v-list two-line subheader>
+          <v-subheader>Текущий рейтинг</v-subheader>
+          <v-list-tile
+            avatar
+            v-for="channel in channels"
+            :key="channel.name"
+
+            :class="{ 'amber lighten-1': channel.isMy }"
+          >
+            <v-list-tile-action v-if="!isMyChannelExists">
+              <v-btn icon @click="markChannelAsMine(channel)">
+                <v-icon>{{`${channel.isMy ? 'thumb_up' : 'thumb_down'}`}}</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{channel.subs}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{channel.name}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
+</div>
 </template>
 
 <script>
 import axios from 'axios';
 
 class User {
-  constructor(name, subs, isMe = false) {
+  constructor(name, subs, isMy = false) {
     this.name = name;
     this.subs = subs;
-    this.isMe = isMe;
+    this.isMy = isMy;
   }
 }
 
@@ -42,12 +63,29 @@ export default {
       searchRules: [
         v => !!v || (this.isUsername ? 'Укажите имя пользователя' : 'Укажите ID канала'),
       ],
-      channels: [],
       isUsername: true,
+      isMyChannelExists: false,
+      myChannelName: null,
     };
   },
-  created() {},
+  computed: {
+    selectedChannel() {
+      return this.channels.find(channel => channel.name === this.myChannelName);
+    },
+  },
+  created() {
+    const channels = JSON.parse(localStorage.getItem('channels'));
+
+    if (channels) {
+      this.channels = channels;
+    }
+  },
   methods: {
+    markChannelAsMine(channel) {
+      channel.isMy = true;
+      this.myChannelName = channel.name;
+      this.isMyChannelExists = true;
+    },
     getChannelData: async function(search) {
       try {
         const apiKey = 'AIzaSyAGC8ADxI5-M2yoi4Pt_AHE2FpaH2Y6xpM';
