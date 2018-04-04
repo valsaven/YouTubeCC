c8cb9290c7941b2da0c03749d3ff3ba40942945d
Val Saven <val.saven@gmail.com>
2018-04-04 14:10:02 +0300

    Initial files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'YouTubeCC',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
