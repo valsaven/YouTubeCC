903f1f08ddbde08992f5ba059f8c92f557deaa52
Val Saven <val.saven@gmail.com>
2018-04-10 22:55:08 +0300

    Add alertBox component

--- src/components/addChannelForm.vue
+++ src/components/addChannelForm.vue
@@ -2,18 +2,16 @@
   <v-form v-model="valid" ref="form" lazy-validation>
     <v-text-field label="ID канала" v-model="search" :rules="searchRules" required></v-text-field>
     <v-btn @click="submit" :disabled="!valid">Добавить</v-btn>
-    <v-alert type="success" dismissible v-model="success.alert" transition="scale-transition">
-      {{success.message}}
-    </v-alert>
-    <v-alert type="error" dismissible v-model="error.alert" transition="scale-transition">
-      {{error.message}}
-    </v-alert>
+    <alert-box v-bind="{success, error}"></alert-box>
   </v-form>
 </template>
 
 <script>
+import alertBox from './alertBox';
+
 export default {
   name: 'add-channel-form',
+  components: { alertBox },
   props: ['success', 'error'],
   data() {
     return {
---
+++ src/components/alertBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="alert-box">
+    <v-alert type="success" dismissible v-model="success.alert" transition="scale-transition">
+      {{success.message}}
+    </v-alert>
+    <v-alert type="error" dismissible v-model="error.alert" transition="scale-transition">
+      {{error.message}}
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'alert-box',
+  props: ['success', 'error'],
+};
+</script>
